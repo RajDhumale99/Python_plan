4c4696c4c237c2668caa5182205cf1fc44812a99
RajDhumale99 <128817954+RajDhumale99@users.noreply.github.com>
2025-05-05 18:26:29 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,32 @@
+# # if-else conditions:
+
+# # rollercoaster, min height required
+
+# height= int(input("enter height"))
+# age = int(input("enter age"))
+# bill = 0
+
+# if height >= 120:
+#     print("can enter")
+#     if age < 12:
+#         bill += 50
+#         print("pay Rs.50")
+#     elif 12 <= age <= 18: # 12 -18
+#         bill += 100 
+#         print("pay Rs. 100")     
+#     else:
+#         bill += 200
+#         print("pay Rs. 200") 
+
+#     photo = input("want photo? Y or N")
+#     if photo == "Y":
+#         bill += 50
+#         print(f"pay Rs.{bill}")
+#     else:
+#         print(f"Final bill Rs.{bill}")
+# else:
+#     print("Too short to ride")
+
+# #  
+
+print(10 % 3)
